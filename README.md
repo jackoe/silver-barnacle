505e688c315dfb25b4d66708ea76fc9b85218a89
Jack Wines <jackwines@mac.com>
2020-05-04 22:38:40 -0400

    Skeleton for article typee and Jinja templates

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ python3 -m pip3 install -U mypy
 # then install flask:
 python3 -m pip3 install -U flask
 # then run
-python3 src/main.py
+env FLASK_APP=main.py flask run
 ```
--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
+from flask import render_template
 from flask import Flask
 
 app = Flask(__name__)
 
+class article:
+    def __init__(self, url):
+        self.url = url
+        # note: change to grab the title by making a GET request
+        self.title = "title here"
+
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def urls():
+    articles = [article("https://winesj.com"), article("https://connellyj.github.io/")]
+    return render_template("index.html", articles = articles)
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
 
 if __name__ == '__main__':
     app.run()
